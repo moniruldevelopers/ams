82dd7d8b3b5aa42593500334baf0334c569bee39
moniruldevelopers <monirul.developers@gmail.com>
2024-12-30 21:58:24 +0600

    f

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^omptdk42@3c8$m9u6dpn6tlrs(dqpspqa#m@l-^g=%3tcnixg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
